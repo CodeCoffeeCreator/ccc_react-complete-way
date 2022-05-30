d7f31b4a70048faed87d09bf1f233c589c13ff26
Denny Pearce <digital.y.coffee@gmail.com>
2022-05-30 22:53:55 +0300

    create react app for hooks deep dive course, clear the template

---
+++ hooks-deep-dive/src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+
+import { MasterDetails } from './MasterDetail/MasterDetails';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<MasterDetails />);
