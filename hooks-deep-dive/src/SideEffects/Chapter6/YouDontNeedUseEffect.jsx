5c53494e89f1331f48a46c12e0e920f4a998c0ec
Denny Pearce <digital.y.coffee@gmail.com>
2022-06-01 01:24:54 +0300

    hooks-deep-dive -> realize code for second block course - useEffect hook etc

---
+++ hooks-deep-dive/src/SideEffects/Chapter6/YouDontNeedUseEffect.jsx
@@ -0,0 +1,71 @@
+import { useEffect, useState } from 'react';
+import { Toolbar } from 'components/Toolbar';
+import { Button } from 'components/Button';
+import { useRerender } from 'hooks/useRerender';
+
+function useBallPosition(step) {
+  const [left, setLeft] = useState(0);
+  const [top, setTop] = useState(0);
+
+  useEffect(() => {
+    const handleKeyDown = (event) => {
+      switch (event.key) {
+        case 'ArrowLeft':
+          setLeft((prev) => Math.max(prev - step, 0));
+          break;
+        case 'ArrowRight':
+          setLeft((prev) => prev + step);
+          break;
+        case 'ArrowUp':
+          setTop((prev) => Math.max(prev - step, 0));
+          break;
+        case 'ArrowDown':
+          setTop((prev) => prev + step);
+          break;
+        default:
+          break;
+      }
+    };
+
+    document.addEventListener('keydown', handleKeyDown);
+
+    return () => {
+      document.removeEventListener('keydown', handleKeyDown);
+    };
+  }, [step]);
+
+  return [left, top];
+}
+
+function buildStyle(left, top) {
+  return {
+    position: 'absolute',
+    fontSize: 64,
+    transition: 'all 0.5s ease-in-out',
+    transform: `rotate(${Math.random() * 360}deg)`,
+    left,
+    top,
+  };
+}
+
+export function YouDontNeedUseEffect() {
+  const [left, top] = useBallPosition(50);
+  const style = buildStyle(left, top);
+  const rerender = useRerender();
+
+  return (
+    <>
+      <h2>Chapter 6. useEffect</h2>
+      <h3>You don't need useEffect</h3>
+
+      <Toolbar>
+        <Button text='Click me to re-render the component' onClick={rerender} />
+      </Toolbar>
+
+      <div>Use arrow keys to move the ball.</div>
+      <div style={{ position: 'relative' }}>
+        <div style={style}>⚽️</div>
+      </div>
+    </>
+  );
+}
--- hooks-deep-dive/src/SideEffects/SideEffectsSection.jsx
+++ hooks-deep-dive/src/SideEffects/SideEffectsSection.jsx
@@ -1,7 +1,28 @@
-import React from 'react';
+import { Section, Chapter } from 'components/MasterDetail';
+import { UseEffectBasics } from './Chapter6/UseEffectBasics';
+import { UseEffectAndInterval } from './Chapter6/UseEffectAndInterval';
+import { YouDontNeedUseEffect } from './Chapter6/YouDontNeedUseEffect';
+import { EffectsSequence } from './Chapter7/EffectsSequence';
+import { UseLayoutEffectExample } from './Chapter7/UseLayoutEffectExample';
 
-const SideEffectsSection = () => {
-  return <div>SideEffectsSection</div>;
-};
-
-export default SideEffectsSection;
+export function SideEffectsSection() {
+  return (
+    <Section title='Side effects, useEffect'>
+      <Section title='6. useEffect'>
+        <Chapter title='useEffect basics' component={UseEffectBasics} />
+        <Chapter
+          title='useEffect + setInterval'
+          component={UseEffectAndInterval}
+        />
+        <Chapter
+          title="You don't need useEffect"
+          component={YouDontNeedUseEffect}
+        />
+      </Section>
+      <Section title='7. useLayoutEffect'>
+        <Chapter title='Effects sequence' component={EffectsSequence} />
+        <Chapter title='Batching effects' component={UseLayoutEffectExample} />
+      </Section>
+    </Section>
+  );
+}
