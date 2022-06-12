3e793d9e7504b5e0aac88d9785dd40f51636b2cb
Denny Pearce <digital.y.coffee@gmail.com>
2022-06-12 23:36:03 +0300

    write components for fortnite api shop parser

---
+++ fortnite-shop/src/config.js
@@ -0,0 +1,6 @@
+// const API_KEY = process.env.REACT_APP_API_KEY;
+const API_KEY = 'afabdc82-15909de1-a8d63927-3431a420';
+
+const API_URL = 'https://fortniteapi.io/v1/shop?lang=en';
+
+export { API_KEY, API_URL };
